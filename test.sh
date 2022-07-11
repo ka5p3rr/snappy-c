12c16d8d61542e26b2458fbb2c3ab73cb21b4a59
David Mares <32355403+ka5p3rr@users.noreply.github.com>
2022-07-11 16:16:47 +0100

    add custom list of files

--- test.sh
+++ test.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 
-for filename in $(find compression-corpus \( ! -regex '.*/\..*' \) -type f -follow); do
+files=(
+    "compression-corpus/canterbury/alice29.txt"
+    "compression-corpus/canterbury/asyoulik.txt"
+    "compression-corpus/canterbury/cp.html"
+    "compression-corpus/canterbury/fields.c"
+    "compression-corpus/canterbury/grammar.lsp"
+    "compression-corpus/canterbury/kennedy.xls"
+    "compression-corpus/canterbury/lcet10.txt"
+    "compression-corpus/canterbury/plrabn12.txt"
+    "compression-corpus/canterbury/ptt5"
+    "compression-corpus/canterbury/sum"
+    "compression-corpus/canterbury/xargs.1"
+    "compression-corpus/enwik/enwik8.txt"
+    "compression-corpus/silesia/dickens"
+    "compression-corpus/silesia/mozilla"
+    "compression-corpus/silesia/mr"
+    "compression-corpus/silesia/nci"
+    "compression-corpus/silesia/ooffice"
+    "compression-corpus/silesia/osdb"
+    "compression-corpus/silesia/reymont"
+    "compression-corpus/silesia/samba"
+    "compression-corpus/silesia/sao"
+    "compression-corpus/silesia/webster"
+    "compression-corpus/silesia/x-ray"
+    "compression-corpus/silesia/xml"
+)
+
+for filename in "${files[@]}"; do
     ./verify -v $filename
 done
