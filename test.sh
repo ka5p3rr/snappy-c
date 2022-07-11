eb306ed9e8b81ff71f81982ceb347605cd387b3e
David Mares <32355403+ka5p3rr@users.noreply.github.com>
2022-07-11 16:05:21 +0100

    ignore hidden files in test

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-for filename in $(find compression-corpus -type f -follow); do
+for filename in $(find compression-corpus \( ! -regex '.*/\..*' \) -type f -follow); do
     ./verify -v $filename
 done
